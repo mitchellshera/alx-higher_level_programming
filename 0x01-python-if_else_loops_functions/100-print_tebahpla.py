9214821a3c45440f2b030cbe85ea787ce887fe98
mitchellshera <64257596+mitchellshera@users.noreply.github.com>
2023-05-10 00:30:35 +0300

    done

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,9 +2,9 @@
 for i in range(ord('z'), ord('a') - 1, -1):
         letter = chr(i)
             if (i - ord('a')) % 2 == 0:
-                    letter = letter.upper()
-                        else:
-                                letter = letter.lower()
-                                    print("{}".format(letter), end='')
+               letter = letter.upper()
+            else:
+               letter = letter.lower()
+               print("{}".format(letter), end='')
 
-                                    print("")
+            print("")
