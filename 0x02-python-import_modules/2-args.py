80323948e9025336ff4ca3589ad480746c3c8804
mitchellshera <mitchellwachera@gmail.com>
2023-05-11 23:09:44 +0300

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-
-    n = len(argv) - 1
-
-    if n == 0:
-        print("No arguments.")
-    else:
-        argument_plural = "s" if n > 1 else ""
-        print("{} argument{}{}:".format(n, argument_plural, ":"))
-
-        for i, arg in enumerate(argv[1:], start=1):
-            print("{}: {}".format(i, arg))
+    m = len(argv)
+    print("{:d} {:s}{:s}".format(m - 1, "argument" if m <= 2 else "arguments",
+                                 "." if m == 1 else ":"))
+    for i, s in enumerate(argv):
+        if i > 0:
+            print("{:d}: {:s}".format(i, s))
