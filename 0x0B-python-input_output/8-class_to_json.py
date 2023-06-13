2bb3309085c8d48458068c35e182463c49128dd3
mitchellshera <mitchellwachera@gmail.com>
2023-06-13 21:20:32 +0300

    11

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""DEfines a python class-to-JSON function"""
+
+def class_to_json(obj):
+    """Converts an object to a JSON-serializable dictionary representation."""
+    json_dict = {}
+
+    # Iterate through all attributes of the object
+    for attr, value in obj.__dict__.items():
+        # Check if the value is a list, dictionary, string, integer, or boolean
+        if isinstance(value, (list, dict, str, int, bool)):
+            json_dict[attr] = value
+
+    return json_dict
