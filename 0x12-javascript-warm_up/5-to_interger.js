d8882b03c5b7d9700634a160f5dc2bae03a85b97
mitchellshera <mitchellwachera@gmail.com>
2023-08-14 15:23:04 +0300

    task 5

---
+++ 0x12-javascript-warm_up/5-to_interger.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const [,, arg1] = process.argv;
+
+const myArg = parseInt(arg1);
+
+if (!isNaN(myArg)) {
+  console.log(`My number: ${myArg}`);
+} else {
+  console.log("Not a number");
+}
