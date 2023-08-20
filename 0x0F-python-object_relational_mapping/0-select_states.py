d66c1c598de3bc9974aae8cfedd93844acd6c960
mitchellshera <mitchellwachera@gmail.com>
2023-08-20 13:19:57 +0300

    0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,36 +1,39 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 ''' Script to select all the states from a database '''
 
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: ./0-select_states.py <mysql username> <mysql password> <database name>")
-        sys.exit(1)
 
+def show():
+    '''
+    Gets MySQL username, password and database from the user input
+    '''
     username = sys.argv[1]
     password = sys.argv[2]
-    db_name = sys.argv[3]
+    database = sys.argv[3]
+    host = 'localhost'
+    port = 3306
 
-    try:
-        db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=username,
-            passwd=password,
-            db=db_name
-        )
-        
-        cursor = db.cursor()
-        cursor.execute("SELECT * FROM states ORDER BY id ASC;")
-        states = cursor.fetchall()
+    '''connect to the database given '''
+    conn = MySQLdb.connect(host=host, port=port, user=username,
+                           password=password, db=database)
 
-        for state in states:
-            print(state)
-            
-        cursor.close()
-        db.close()
+    ''' A cursor object '''
+    cursor = conn.cursor()
 
-    except MySQLdb.Error as e:
-        print("MySQL Error:", e)
+    '''
+    SELECT statement that gets all states from database
+    '''
+    cursor.execute('SELECT * FROM states ORDER BY states.id ASC')
+
+    ''' Print the results found '''
+    for row in cursor:
+        print(row)
+
+    ''' Close the connection '''
+    conn.close()
+
+
+if __name__ == '__main__':
+    show()
