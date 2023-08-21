de9e9f61ac32248fd2159971c341abb3e968c890
mitchellshera <mitchellwachera@gmail.com>
2023-08-21 16:54:46 +0300

    3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+'''
+A script that takes in an argument and displays
+all values where name matches the argument
+'''
+
+import MySQLdb
+import sys
+
+
+def display_values():
+    '''displays values where name matches arg '''
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+    host = 'localhost'
+    port = 3306
+
+    conn = MySQLdb.connect(host=host,
+                           port=port,
+                           user=username,
+                           passwd=password,
+                           db=database)
+
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM states WHERE name = \
+                   %s ORDER BY id ASC; ",
+                   [state_name])
+
+    results = cursor.fetchall()
+
+    for state in results:
+        print(state)
+
+    cursor.close()
+    conn.close()
+
+
+if __name__ == '__main__':
+    display_values()
