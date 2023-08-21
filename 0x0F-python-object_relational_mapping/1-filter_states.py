2fd474fba14b874adda4b48e7c1c914c8d9fb71c
mitchellshera <mitchellwachera@gmail.com>
2023-08-21 16:19:43 +0300

    111

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,29 +6,27 @@ import MySQLdb
 import sys
 
 
-def filter_states_with_n():
-    '''Lists all states with names starting with N'''
+def lists_N():
+    '''lists all states with a name that starts with N'''
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
     host = 'localhost'
     port = 3306
 
-    try:
-        db = MySQLdb.connect(host=host, user=username, passwd=password,
-                             db=db_name, port=port)
-        cur = db.cursor()
-        cur.execute('SELECT * FROM states WHERE name LIKE "N%" ' +
-                    'ORDER BY states.id ASC')
-        result = cur.fetchall()
-        cur.close()
-        db.close()
-        
+    db = MySQLdb.connect(host=host, user=username, passwd=password,
+                         db=db_name, port=port)
+    cur = db.cursor()
+    cur.execute('SELECT * FROM states WHERE name regexp "^N.*" ' +
+                'ORDER BY states.id ASC')
+    result = cur.fetchall()
+    cur.close()
+    db.close()
+    if result:
         for state in result:
-            print(state)
+            if state[1][0] == "N":
+                print(state)
 
-    except MySQLdb.Error as e:
-        print("MySQL Error:", e)
 
 if __name__ == "__main__":
-    filter_states_with_n()
+    lists_N()
