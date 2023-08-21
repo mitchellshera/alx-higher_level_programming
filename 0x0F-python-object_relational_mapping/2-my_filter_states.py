60cf015a236c78f909a323816ec97b33037488e2
mitchellshera <mitchellwachera@gmail.com>
2023-08-21 16:46:29 +0300

    2-cor

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,8 +7,8 @@ import MySQLdb
 import sys
 
 
-def filter_states_by_name():
-    '''Displays values where name matches the argument'''
+def display_tables():
+    '''displays values where name matches arg '''
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
@@ -16,28 +16,21 @@ def filter_states_by_name():
     host = 'localhost'
     port = 3306
 
-    try:
-        conn = MySQLdb.connect(host=host,
-                               port=port,
-                               user=username,
-                               passwd=password,
-                               db=database)
+    conn = MySQLdb.connect(host=host,
+                           port=port,
+                           user=username,
+                           passwd=password,
+                           db=database)
 
-        cursor = conn.cursor()
-        query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC;"
-        cursor.execute(query, (state_name,))
-
-        results = cursor.fetchall()
-
-        ''' Print the results '''
-        for state in results:
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY \
+    id ASC".format(sys.argv[4]))
+    states = cursor.fetchall()
+    for state in states:
+        if state[1] == sys.argv[4]:
             print(state)
-
-        cursor.close()
-        conn.close()
-
-    except MySQLdb.Error as e:
-        print("MySQL Error:", e)
+    cursor.close()
+    conn.close()
 
 if __name__ == '__main__':
-    filter_states_by_name()
+    display_tables()
