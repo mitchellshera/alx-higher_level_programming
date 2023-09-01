2cfed8ffca6ae071d49af39a8e00bc5cd3964a9b
mitchellshera <mitchellwachera@gmail.com>
2023-09-02 00:49:29 +0300

    6-8

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -2,7 +2,8 @@
 
 """
 script that takes in a URL and an email address,
-sends a POST request to the passed URL with the email as a parameter, and finally displays the body of the response.
+sends a POST request to the passed URL with the email as
+a parameter, and finally displays the body of the response.
 """
 
 import requests
@@ -15,6 +16,6 @@ if __name__ == "__main__":
     data = {'email': email}
 
     response = requests.post(url, data=data)
-    
+
     print("Your email is:", email)
     print(response.text)
