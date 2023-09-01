4ae45e31103aa96b1637423db11c558691a179d0
mitchellshera <mitchellwachera@gmail.com>
2023-09-02 00:20:36 +0300

    4-6

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,7 +5,7 @@
 import requests
 
 if __name__ == "__main__":
-    response = requests.get("https://alx-intranet.hbtn.io/status")
+    req = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
-    print("\t- type: {}".format(type(response.text)))
-    print("\t- content: {}".format(response.text))
+    print("\t- type: {}".format(type(req.text)))
+    print("\t- content: {}".format(req.text))
