df5b9a52a4f31250f89807363cd1a69b1cac0d57
mitchellshera <mitchellwachera@gmail.com>
2023-09-01 23:54:48 +0300

    task 3-5 and task 1 corr

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+Script that takes a request, sends it and shows error messages
+"""
+
+if __name__ == "__main__":
+    import sys
+    from urllib import request, error
+
+    try:
+        with request.urlopen(sys.argv[1]) as response:
+            print(response.read().decode('UTF-8'))
+    except error.HTTPError as err:
+        print('Error code:', err.code)
