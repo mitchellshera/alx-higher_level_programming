f2df5607d98afd1a1ceb97f74e8937d03d7942a2
mitchellshera <mitchellwachera@gmail.com>
2023-09-01 23:33:57 +0300

    task 00

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-""" script that fetches a url"""
+""" script that fetches a https://alx-intranet.hbtn.io/status"""
 
 import urllib.request
 
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    content = response.read()
-    print("Body response:")
-    print("    - type:", type(content))
-    print("    - content:", content)
+try:
+    with urllib.request.urlopen(https://alx-intranet.hbtn.io/status) as response:
+        body = response.read()
+        utf8_content = body.decode('utf-8')
+
+        print("Body response:")
+        print("    - type:", type(body))
+        print("    - content:", body)
+        print("    - utf8 content:", utf8_content)
+except Exception as e:
+    print("Error:", e)
