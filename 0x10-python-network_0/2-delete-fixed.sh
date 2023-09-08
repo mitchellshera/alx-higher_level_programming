5114f9f11811ad7ba9b2792b2970c88981261638
mitchellshera <mitchellwachera@gmail.com>
2023-09-08 13:06:07 +0300

    corrr

--- 0x10-python-network_0/2-delete-fixed.sh
+++ 0x10-python-network_0/2-delete-fixed.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Sends a DELETE request to a given URL and display the response body
 curl -s -X DELETE "$1"
 
--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Displays all HTTP methods.
 curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
 
