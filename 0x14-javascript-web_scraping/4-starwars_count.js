116af02e4d67903db9bbeaeca990a482eedb66b7
mitchellshera <mitchellwachera@gmail.com>
2023-09-27 17:08:18 +0300

    4-5

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,13 +2,13 @@
 const request = require('request');
 request(process.argv[2],
 
-function (error,response, body) {
-  if (!error) {
-    const results = JSON.parse(body).results;
-    console.log(results.reduce((count, movie) => {
-      return movie.characters.find((character) => character.endsWith('/18/'))
-        ? count + 1
-        : count;
-    }, 0));
-  }
-});
+  function (error, response, body) {
+    if (!error) {
+      const results = JSON.parse(body).results;
+      console.log(results.reduce((count, movie) => {
+        return movie.characters.find((character) => character.endsWith('/18/'))
+          ? count + 1
+          : count;
+      }, 0));
+    }
+  });
