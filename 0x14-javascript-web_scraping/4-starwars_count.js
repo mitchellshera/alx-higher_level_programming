5705a75c5f9493159e5066a78745cd1899f6d04e
mitchellshera <mitchellwachera@gmail.com>
2023-09-27 16:54:45 +0300

    Update 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,29 +1,14 @@
 #!/usr/bin/node
 const request = require('request');
-const apiUrl = process.argv[2];
-const wedgeAntillesCharacterId = 18;
+request(process.argv[2],
 
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(`Error making API request: ${error}`);
-    process.exit(1);
-  }
-
-  if (response.statusCode !== 200) {
-    console.error(`Error: Received HTTP status code ${response.statusCode}`);
-    process.exit(1);
-  }
-
-  try {
-    const data = JSON.parse(body);
-
-    const moviesWithWedgeAntilles = data.results.filter((movie) =>
-      movie.characters.includes(wedgeAntillesCharacterId)
-    );
-
-    console.log(`Number of movies with Wedge Antilles: ${moviesWithWedgeAntilles.length}`);
-  } catch (parseError) {
-    console.error(`Error parsing API response: ${parseError}`);
-    process.exit(1);
+function (error, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
   }
 });
