5ed949d6c417b9a835fd78783f98310d13b4e98c
mitchellshera <mitchellwachera@gmail.com>
2023-09-27 17:37:10 +0300

    Update 6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,8 +1,16 @@
 #!/usr/bin/node
 
 const request = require('request');
+
+// Check if the correct number of command-line arguments are provided
+if (process.argv.length !== 3) {
+  console.error('Usage: ./6-completed_tasks.js <API_URL>');
+  process.exit(1);
+}
+
 const apiUrl = process.argv[2];
 
+// Make an HTTP GET request to the specified API URL
 request(apiUrl, (error, response, body) => {
   if (error) {
     console.error(`Error making API request: ${error}`);
@@ -16,9 +24,12 @@ request(apiUrl, (error, response, body) => {
 
   try {
     const tasks = JSON.parse(body);
+
+    // Create an object to store the counts of completed tasks by user ID
     const completedTasksByUser = {};
 
-    for (const task of tasks) {
+    // Iterate through the tasks and count completed tasks by user ID
+    tasks.forEach((task) => {
       if (task.completed) {
         if (completedTasksByUser[task.userId]) {
           completedTasksByUser[task.userId]++;
@@ -26,11 +37,10 @@ request(apiUrl, (error, response, body) => {
           completedTasksByUser[task.userId] = 1;
         }
       }
-    }
+    });
 
-    for (const userId in completedTasksByUser) {
-      console.log(`User ID ${userId}: ${completedTasksByUser[userId]} completed tasks`);
-    }
+    // Print the completed tasks by user ID as a JSON object
+    console.log(completedTasksByUser);
   } catch (parseError) {
     console.error(`Error parsing API response: ${parseError}`);
     process.exit(1);
