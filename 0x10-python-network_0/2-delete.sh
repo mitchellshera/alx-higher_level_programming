ee0b171d86381628dc269481ba8e9c9fc3575412
mitchellshera <mitchellwachera@gmail.com>
2023-10-04 13:33:48 +0300

    0

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# sends DELETE req to $1 URL and display response body
-curl -s "$1" -X DELETE
+# Sends a DELETE request to a given URL and display the response body
+curl -s -X DELETE "$1"
+
